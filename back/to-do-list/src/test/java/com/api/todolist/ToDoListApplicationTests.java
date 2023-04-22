a22f86d57c54dec4903761befc7bea6cc19c1b66
Lucas Martins <lucas.martins559@outlook.com>
2023-04-22 20:23:08 -0300

    Primeiro Commit Backend

---
+++ back/to-do-list/src/test/java/com/api/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
